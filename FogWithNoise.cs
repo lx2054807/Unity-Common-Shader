e6501b8425c30a5c65496bae042467ef5c95acd4
xinli <lixin@xd.com>
2020-12-18 21:39:40 +0800

    不均匀的雾

---
+++ FogWithNoise.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogWithNoise : PostEffectsBase
+{
+    public Shader fogShader;
+    private Material fogMaterial;
+    public Material material
+    {
+        get
+        {
+            fogMaterial = CheckShaderAndCreateMaterial(fogShader, fogMaterial);
+            return fogMaterial;
+        }
+    }
+
+    private Camera myCamera;
+    public Camera camera
+    {
+        get
+        {
+            if (myCamera == null)
+            {
+                myCamera = GetComponent<Camera>();
+            }
+            return myCamera;
+        }
+    }
+
+    private Transform myCameraTransform;
+    public Transform cameraTransform
+    {
+        get
+        {
+            if (myCameraTransform == null)
+            {
+                myCameraTransform = camera.transform;
+            }
+            return myCameraTransform;
+        }
+    }
+
+    [Range(0.0f, 3.0f)]
+    public float fogDensity = 1.0f;
+
+    public Color fogColor = Color.white;
+
+    public float fogStart = -1.0f;
+    public float fogEnd = 1.0f;
+
+    public Texture noiseTexture;
+
+    [Range(-0.5f, 0.5f)] public float fogXSpeed = 0.1f;
+    [Range(-0.5f, 0.5f)] public float fogYSpeed = 0.1f;
+    [Range(0.0f, 3.0f)] public float noiseAmount = 1.0f;
+
+    private void OnEnable()
+    {
+        camera.depthTextureMode |= DepthTextureMode.Depth;
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (material != null)
+        {
+            Matrix4x4 frustumCorners = Matrix4x4.identity;
+
+            float fov = camera.fieldOfView;
+            float near = camera.nearClipPlane;
+            float far = camera.farClipPlane;
+            float aspect = camera.aspect;
+
+            float halfHeight = near * Mathf.Tan(fov * 0.5f * Mathf.Deg2Rad);
+            Vector3 toRight = cameraTransform.right * halfHeight * aspect;
+            Vector3 toTop = cameraTransform.up * halfHeight;
+
+            Vector3 topLeft = cameraTransform.forward * near + toTop - toRight;
+            float scale = topLeft.magnitude / near;
+            topLeft.Normalize();
+            topLeft *= scale;
+
+            Vector3 topRight = cameraTransform.forward * near + toRight + toTop;
+            topRight.Normalize();
+            topRight *= scale;
+
+            Vector3 botLeft = cameraTransform.forward * near - toRight - toTop;
+            botLeft.Normalize();
+            botLeft *= scale;
+
+            Vector3 botRight = cameraTransform.forward * near + toRight - toTop;
+            botRight.Normalize();
+            botRight *= scale;
+
+            frustumCorners.SetRow(0, botLeft);
+            frustumCorners.SetRow(1, botRight);
+            frustumCorners.SetRow(2, topRight);
+            frustumCorners.SetRow(3, topLeft);
+
+            material.SetMatrix("_FrustumCornersRay", frustumCorners);
+            material.SetMatrix("_ViewProjectionInverseMatrix", (camera.projectionMatrix * camera.worldToCameraMatrix).inverse);
+
+            material.SetFloat("_FogDensity", fogDensity);
+            material.SetFloat("_FogStart", fogStart);
+            material.SetFloat("_FogEnd", fogEnd);
+            material.SetColor("_FogColor", fogColor);
+            
+            material.SetTexture("_NoiseTexture", noiseTexture);
+            material.SetFloat("_FogXSpeed", fogXSpeed);
+            material.SetFloat("_FogYSpeed", fogYSpeed);
+            material.SetFloat("_NoiseAmount", noiseAmount);
+
+            Graphics.Blit(source, destination, material);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+}
---
+++ FogWithNoise.shader
@@ -0,0 +1,104 @@
+﻿Shader "Unlit/FogWithNoise"
+{
+    Properties
+    {
+        _MainTex("Texture", 2D) = "white" {}
+        _FogDensity("_FogDensity", float) = 1.0
+        _FogColor("Fog Color", color) = (1,1,1,1)
+        _FogStart("Fog Start", float) = -1.0
+        _FogEnd("Fog End",float) = 1.0
+        _FogXSpeed("Fog X Speed", float) = 0.1
+        _FogYSpeed("Fog Y Speed", float) = 0.1
+        _NoiseTexture("NoiseTexture",2D) = "white" {}
+        _NoiseAmount("NoiseAmount", float) = 1.0
+    }
+        SubShader
+        {
+            CGINCLUDE
+            #include "UnityCG.cginc"
+
+            sampler2D _MainTex;
+            half4 _MainTex_TexelSize;
+            sampler2D _CameraDepthTexture;
+            float4x4 _FrustumCornersRay;
+            half _FogDensity;
+            fixed4 _FogColor;
+            half _FogStart;
+            half _FogEnd;
+            sampler2D _NoiseTexture;
+            float _NoiseAmount;
+            float _FogXSpeed;
+            float _FogYSpeed;
+
+            struct v2f
+            {
+                float4 pos : SV_POSITION;
+                half2 uv : TEXCOORD0;
+                half2 uv_depth : TEXCOORD1;
+                float4 interpolatedRay : TEXCOORD2;
+            };
+
+            v2f vert(appdata_img v)
+            {
+                v2f o;
+                o.pos = UnityObjectToClipPos(v.vertex);
+                o.uv = v.texcoord;
+                o.uv_depth = v.texcoord;
+
+#if UNITY_UV_STARTS_AT_TOP
+                if (_MainTex_TexelSize.y < 0)
+                    o.uv_depth.y = 1 - o.uv_depth.y;
+#endif
+                int index = 0;
+                if (v.texcoord.x < 0.5 && v.texcoord.y < 0.5) {
+                    index = 0;
+                }
+                else if(v.texcoord.x > 0.5 && v.texcoord.y < 0.5)
+                {
+                    index = 1;
+                }
+                else if(v.texcoord.x > 0.5 && v.texcoord.y > 0.5)
+                {
+                    index = 2;
+                }
+                else 
+                {
+                    index = 3;
+                }
+
+#if UNITY_UV_STARTS_AT_TOP
+                if (_MainTex_TexelSize.y < 0)
+                    index = 3 - index;
+#endif
+                o.interpolatedRay = _FrustumCornersRay[index];
+                return o;
+                return o;
+            }
+
+            fixed4 frag(v2f i) : SV_TARGET{
+                float linearDepth = LinearEyeDepth(SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv_depth));
+                float3 worldPos = _WorldSpaceCameraPos + linearDepth * i.interpolatedRay.xyz;
+                float2 speed = _Time.y * float2(_FogXSpeed, _FogYSpeed);
+                float noise = (tex2D(_NoiseTexture, i.uv + speed).r - 0.5) * _NoiseAmount;
+                
+                float fogDensity = (_FogEnd - worldPos.y) / (_FogEnd - _FogStart);
+                fogDensity = saturate(fogDensity * _FogDensity * (1+noise));
+                fixed4 finalColor = tex2D(_MainTex, i.uv);
+                return fixed4(lerp(finalColor.rgb, _FogColor.rgb, fogDensity), 1);
+            }
+            ENDCG
+
+        Pass
+        {
+            ZTest Always
+            Cull Off
+            ZWrite Off
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            ENDCG
+        }
+    }
+    FallBack Off
+}
